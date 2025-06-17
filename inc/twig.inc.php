4281942c312fcdb6a721c0affb77e149614a8417
Alex <Like2968@gmail.com>
2025-06-17 15:59:08 +0800

    remove index.html and add initial db and twig configuration files

---
+++ inc/twig.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+// 只在vendor中使用我們需要的外掛
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+// 設定時區
+date_default_timezone_set('Asia/Taipei');
+
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
+
+// Twig 環境設定
+$twig = new Environment($loader, [
+    'cache' => __DIR__ . '/../cache', 
+    'debug' => true,                
+]);
+
+// 載入 Twig 除錯擴展 (只有在 debug 為 true 時才啟用)
+if ($twig->isDebug()) {
+    $twig->addExtension(new \Twig\Extension\DebugExtension());
+}
