f47985d4d75389fafaf72ad01ced290b81907241
Alex <Like2968@gmail.com>
2025-06-24 02:04:47 +0800

    Enhance user management and login interface: add user role and account details to session data, improve layout and styling of login and user management pages, implement user deletion functionality, and refine form inputs for better user experience.

--- public/switch.php
+++ public/switch.php
@@ -9,25 +9,25 @@ if (isset($_GET['mode']) & $_GET['mode'] != '') {
     $mode = $_GET['mode'];
 }
 
+$data['role'] = $_SESSION['backend_login_role'];
+$data['username'] = $_SESSION['backend_login_name'];
+$data['useracc'] = $_SESSION['backend_login_acc'];
+$data['flag'] = true;
+
 switch($mode) {
-    //編輯清單
     case 'edit':
         $id = $_GET['id'];
         // var_dump($id);
-        $sql = "select * from attendance_log where id = :id";
+        $sql = "SELECT * FROM attendance_log WHERE id = :id";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([":id" => $id]);
         $data['result'] = $stmt->fetch();
-        $data['useracc'] = $_SESSION['backend_login_acc'];
-        $data['role'] = $_SESSION['backend_login_role'];
-        $data['username'] = $_SESSION['backend_login_name'];
         $tmplFile = 'partial/backend/edit.html.twig';
         break;
 
-    //儲存編輯至資料庫
     case 'savedata':
         // die(var_dump($_POST));
-        $stmt = $pdo->prepare("update attendance_log set name = :pname, class_date = :pdate, attended_hours = :patt, leave_early_hours = :pleave, absent_hours = :pabs where id = :pid");
+        $stmt = $pdo->prepare("UPDATE attendance_log SET name = :pname, class_date = :pdate, attended_hours = :patt, leave_early_hours = :pleave, absent_hours = :pabs where id = :pid");
         $stmt->execute(
             [
             ":pname" => $_POST['pname'],
@@ -41,14 +41,10 @@ switch($mode) {
 
         $data["message"] = "您已成功更新".$_POST['pname']."的資料-ID為".$_POST['pid']."的資訊，修改了".$stmt->rowCount()."筆資料";
         $data["alert_type"] = "alert-success";
-        $data['useracc'] = $_SESSION['backend_login_acc'];
-        $data['role'] = $_SESSION['backend_login_role'];
-        $data['username'] = $_SESSION['backend_login_name'];
         $tmplFile = 'partial/message.html.twig';
         break;
 
     case 'show':
-        $data['useracc'] = $_SESSION['backend_login_name'];
         $name = $_GET['name'];
         $stmt = $pdo->prepare("SELECT name, 
                                       round(sum(class_hours), 1) as class_hours, 
@@ -81,8 +77,6 @@ switch($mode) {
             $data['chart']['attended_hours'][] = $record['attended_hours'];
             $data['chart']['raw_hours'][] = $record['raw_hours'];
         }
-        $data['role'] = $_SESSION['backend_login_role'];
-        $data['username'] = $_SESSION['backend_login_name'];
         $tmplFile = 'partial/backend/show_att.html.twig';
         echo $twig->render($tmplFile, $data);
         exit();
@@ -90,18 +84,15 @@ switch($mode) {
     case 'add_data':
         $data['flag'] = true;
         $data['useracc'] = $_SESSION['backend_login_acc'];
-        $data['role'] = $_SESSION['backend_login_role'];
-        $data['username'] = $_SESSION['backend_login_name'];
         $tmplFile = 'partial/backend/add_data.html.twig';
         echo $twig->render($tmplFile, $data);
         exit();
 
     case 'add_stdata':
         // 取得所有學員名稱
-        $stmt = $pdo->prepare('select name from attendance_log group by name;');
+        $stmt = $pdo->prepare('SELECT name FROM attendance_log group BY name;');
         $stmt->execute();
         $nameArray = $stmt->fetchAll(PDO::FETCH_COLUMN);
-        $data['flag'] = true;
 
         // 驗證使用者輸入的學員名稱為班級學員
         if(!in_array($_POST['pname'], $nameArray)) {
@@ -158,23 +149,23 @@ switch($mode) {
             exit();
         }
 
-        $stmt = $pdo->prepare("insert into attendance_log (name, class_date, class_hours, raw_hours, attended_hours, 
-                               late_hours, leave_early_hours, absent_hours) values ( name = :pname, class_date = :pdate, 
-                               class_hours = :pclass, raw_hours = :praw, attended_hours = :patt, late_hours = :plate, 
-                               leave_early_hours = :pleave, absent_hours = :pabs;");
-
-        $stmt->execute(
-            [
-                ":pname" => $_POST['pname'],
-                ":pdate" => $_POST['pdate'],
-                ":pclass" => $_POST['pclass'],
-                ":praw" => (int)$_POST['patt'] + 1.5,
-                ":patt" => $_POST['patt'],
-                ":plate" => $_POST['plate'],
-                ":pleave" => $_POST['pleave'],
-                ":pabs" => $_POST['pabs']
-            ]
+        $stmt = $pdo->prepare(
+            "INSERT INTO attendance_log (name, class_date, class_hours, raw_hours, attended_hours, 
+            late_hours, leave_early_hours, absent_hours) VALUES ( name = :pname, class_date = :pdate, 
+            class_hours = :pclass, raw_hours = :praw, attended_hours = :patt, late_hours = :plate, 
+            leave_early_hours = :pleave, absent_hours = :pabs;"
         );
+
+        $stmt->execute([
+            ":pname" => $_POST['pname'],
+            ":pdate" => $_POST['pdate'],
+            ":pclass" => $_POST['pclass'],
+            ":praw" => (int)$_POST['patt'] + 1.5,
+            ":patt" => $_POST['patt'],
+            ":plate" => $_POST['plate'],
+            ":pleave" => $_POST['pleave'],
+            ":pabs" => $_POST['pabs']
+        ]);
 
         $data["message"] = "您已成功新增學員 ".$_POST['pname']." 於 ".$_POST['pdate']." 的出缺勤資料";
         $data["alert_type"] = "alert-success";
@@ -182,17 +173,17 @@ switch($mode) {
         break;
 
     case 'add_user':
-        $data['flag'] = true;
-        $data['useracc'] = $_SESSION['backend_login_acc'];
-        $data['role'] = $_SESSION['backend_login_role'];
-        $data['username'] = $_SESSION['backend_login_name'];
+        $data['transition'] = 'user';
         $tmplFile = 'partial/backend/add_user.html.twig';
         echo $twig->render($tmplFile, $data);
         exit();
 
-    // 新增使用者
     case 'add_user_data':
         // 取得所有學員名稱
+        $stmt = $pdo->prepare('SELECT acc, user_name FROM admin_user');
+        $stmt->execute();
+        $nameArray = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        $data['transition'] = 'user';
         $acc = $_POST['acc'];
         $pwd = $_POST['pwd'];
         $username = $_POST['username'];
@@ -204,10 +195,7 @@ switch($mode) {
             ":name" => $username
         ]);
     
-
-        $data["message"] = "您已成功新增使用者" . $acc;
-        $data['role'] = $_SESSION['backend_login_role'];
-        $data['username'] = $_SESSION['backend_login_name'];
+        $data["message"] = "您已成功新增使用者-" . $username;
         $data["alert_type"] = "alert-success";
         $tmplFile = 'partial/message.html.twig';
         break;
@@ -225,19 +213,67 @@ switch($mode) {
         $tmplFile = 'partial/message.html.twig';
         break;
 
+    case 'deluser':
+        $data["transition"] = "user";    
+        $stmt = $pdo->prepare("delete from admin_user where no = :id");
+        $stmt->execute(
+            [
+             ":id"=>$_GET['id']
+            ]
+        );
+        $data["message"] = "您已成功刪除該使用者";
+        $data["alert_type"] = "alert-warning";
+        $tmplFile = 'partial/message.html.twig';
+        break;
+
     case 'acc_manage':
-        $data['role'] = $_SESSION['backend_login_role'];
-        $data['username'] = $_SESSION['backend_login_name'];
-        $stmt = $pdo->prepare("SELECT no, acc, pwd, role, user_name FROM `admin_user`;");
-        $stmt->execute();
+        $stmt = $pdo->prepare("SELECT no, acc, pwd, role, user_name FROM `admin_user` WHERE role != :role;");
+        $stmt->execute([
+            ":role" => "admin"
+        ]);
         $acc_results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         foreach($acc_results as $acc) {
             $data['user'][] = $acc;
         }
         $tmplFile = 'partial/backend/acc_manage.html.twig';
-        echo $twig->render($tmplFile, $data);
-        exit();
+        break;
+    
+    case 'edit_user':
+        $id = $_GET['id'];
+        $sql = "SELECT no, acc, pwd, user_name, role FROM admin_user WHERE no = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([":id" => $id]);
+        $data['result'] = $stmt->fetch();
+        if ($_GET['status'] === 'reset') {
+           $data['result']['pwd'] = 'fs101';
+        } 
+        $tmplFile = 'partial/backend/edit_user.html.twig';
+        break;
+        
+    case 'save_user_data':
+        if  ($_POST['role'] != 'admin') {
+            $stmt = $pdo->prepare("UPDATE admin_user SET acc = :acc, pwd = MD5(:pwd), role = :role, user_name = :username where no = :id");
+            $stmt->execute([
+                ":id" => $_POST['id'],
+                ":acc" => $_POST['acc'],
+                ":pwd" => $_POST['pwd'],
+                ":role" => $_POST['role'],
+                ":username" => $_POST['username']
+            ]);
+    
+            $data["message"] = "您已成功更新使用者 " . $_POST['username'] . " 的資料-ID為 " . $_POST['id']." 的資訊，修改了 " . $stmt->rowCount() . " 筆資料";
+            $data["alert_type"] = "alert-success";
+            $data["transition"] = "user";
+            $tmplFile = 'partial/message.html.twig';
+            break;
+        } else {
+            $data["message"] = "系統管理帳號僅此一位，無法新增其他系統管理員";
+            $data["alert_type"] = "alert-danger";
+            $data["transition"] = "user";
+            $tmplFile = 'partial/message.html.twig';
+            break;
+        }
 }
 
 echo $twig->render($tmplFile, $data);
